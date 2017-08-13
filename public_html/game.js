f1b9e20a94a2fde43586a8c8992a6a6b871705f9
Web D.va <webNoDva@gmail.com>
2017-08-13 11:06:21 -0400

    created enemy sprite, added .css

--- public_html/game.js
+++ public_html/game.js
@@ -2,5 +2,35 @@
 
 /* global kontra */
 
-kontra.init();
+kontra.init(); // Initializing the Kontra library.
 
+// Creating an enemy that randomly appears on-screen.
+
+// The sprite for the enemy (its logic will be done elsewhere).
+const randomEnemySprite = kontra.sprite({
+    x: 300,
+    y: 300,
+    width: 32,
+    height: 32,
+    color: 'red'
+});
+
+// Game loop object
+const mainGameLoop = kontra.gameLoop({
+    /*
+     * Used for controlling the game logic.
+     */
+    update: function() {
+        randomEnemySprite.update();
+    },
+    
+    /*
+     * Used for rendering only, it seems.
+     */
+    render: function() {
+        randomEnemySprite.render();
+    }
+});
+
+// Start the game loop.
+mainGameLoop.start();
