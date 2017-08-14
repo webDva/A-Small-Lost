980e37502d3f498bfb5ec6f1edfb35d49d79168c
Web D.va <webNoDva@gmail.com>
2017-08-13 20:20:01 -0400

    Generated random sprites that don't appear.

--- public_html/game.js
+++ public_html/game.js
@@ -6,13 +6,48 @@ kontra.init(); // Initializing the Kontra library.
 
 // Creating an enemy that randomly appears on-screen.
 
-// The sprite for the enemy (its logic will be done elsewhere).
-const randomEnemySprite = kontra.sprite({
-    x: 300,
-    y: 300,
-    width: 32,
-    height: 32,
-    color: 'red'
+// The sprite for the enemy. A custom sprite.
+function CustomRandomEnemySprite(properties) {
+    properties.ttl = 500;
+    properties.width = 32;
+    properties.height = 32;
+    properties.color = 'red';
+
+    // Initialize the sprite.
+    kontra.sprite.prototype.init.call(this, properties);
+}
+CustomRandomEnemySprite.prototype = Object.create(kontra.sprite.prototype);
+
+// Creating custom functions for the custom random enemy sprite, such as shooting bullets.
+CustomRandomEnemySprite.prototype.shootBullets = function () {
+    // Use bullet pattern #2.
+    for (let i = 0; i < 600; i++) {
+        genericBulletPool.get({
+            ttl: defaultGenericBullet.ttl,
+            width: defaultGenericBullet.width,
+            height: defaultGenericBullet.height,
+            color: 'green',
+
+            x: this.x,
+            y: this.y,
+
+            theta: i,
+            update: function () {
+                this.advance();
+                this.theta += i / 360 * Math.PI;
+                this.dx += Math.cos(this.theta) * 0.3;
+                this.dy += Math.sin(this.theta) * 0.3;
+            }
+        });
+    }
+};
+
+// Create an object pool for the random enemy sprite
+const randomEnemyPool = kontra.pool({
+    create: function () {
+        return new CustomRandomEnemySprite({});
+    },
+    maxSize: 9000
 });
 
 // Create a recycling generic bullet particle that will be used by both player and enemies.
@@ -35,30 +70,19 @@ const mainGameLoop = kontra.gameLoop({
      * Used for controlling the game logic.
      */
     update: function () {
-        randomEnemySprite.update();
-
-        // Create a bullet pattern.
-        for (let i = 0; i < 600; i++) {
-            genericBulletPool.get({
-                ttl: defaultGenericBullet.ttl,
-                width: defaultGenericBullet.width,
-                height: defaultGenericBullet.height,
-                color: defaultGenericBullet.color,
-
-                x: kontra.canvas.width / 2,
-                y: kontra.canvas.height / 2,
-                dx: 0,
-                dy: 0,
-                theta: i,
-                update: function () { 
+        // Generate random enemies.
+        for (let i = 0; i < 20; i++) {
+            randomEnemyPool.get({
+                x: Math.floor(Math.random() * (Math.floor(kontra.canvas.width) - Math.ceil(-kontra.canvas.width))) + Math.ceil(-kontra.canvas.width),
+                y: Math.floor(Math.random() * (Math.floor(kontra.canvas.height) - Math.ceil(-kontra.canvas.height))) + Math.ceil(-kontra.canvas.height),
+                update: function () {
                     this.advance();
-                    this.theta += i / 360 * Math.PI;
-                    this.dx += Math.cos(this.theta) * 0.3;
-                    this.dy += Math.sin(this.theta) * 0.3;
+                    this.shootBullets();
                 }
             });
         }
 
+        randomEnemyPool.update();
         genericBulletPool.update();
     },
 
@@ -66,8 +90,7 @@ const mainGameLoop = kontra.gameLoop({
      * Used for rendering only, it seems.
      */
     render: function () {
-        randomEnemySprite.render();
-
+        randomEnemyPool.render();
         genericBulletPool.render();
     }
 });
