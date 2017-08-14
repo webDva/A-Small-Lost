50f1dbe898e646abe4df91936ec5fc716bcfc7dd
Web D.va <webNoDva@gmail.com>
2017-08-14 11:58:00 -0400

    Implemented random box choice.

--- public_html/game.js
+++ public_html/game.js
@@ -12,18 +12,12 @@ const WIDTH_AND_HEIGHT = 64;
 // Super-like class to function as elements in the playing field array.
 class ExistenceBox {
     constructor(xPosition) {
-        this.x = xPosition;
-        this.y = kontra.canvas.height / 2;
-        this.color = 'red';
-        this.width = WIDTH_AND_HEIGHT;
-        this.height = WIDTH_AND_HEIGHT;
-        
         this.sprite = kontra.sprite({
-            x: this.x,
-            y: this.y,
-            color: this.color,
-            width: this.width,
-            height: this.height
+            x: xPosition,
+            y: kontra.canvas.height / 2,
+            color: 'red',
+            width: WIDTH_AND_HEIGHT,
+            height: WIDTH_AND_HEIGHT
         });
     }
 }
@@ -33,12 +27,17 @@ for (let i = 0; i < playingFieldArray.length; i++) {
     playingFieldArray[i] = new ExistenceBox((i + WIDTH_AND_HEIGHT) * i);
 }
 
+// Player's chosen box in the playing field. Initially random.
+let chosenBox = Math.floor(Math.random() * (Math.floor(playingFieldArray.length) - Math.ceil(0))) + Math.ceil(0);
+playingFieldArray[chosenBox].sprite.color = 'green';
+
 // Game loop object
 const mainGameLoop = kontra.gameLoop({
     /*
      * Used for controlling the game logic.
      */
     update: function () {
+        // Why update the sprites that don't do anything? Can this be removed?
         playingFieldArray.forEach((item) => {
             item.sprite.update();
         });
