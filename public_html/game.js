61a13811f9f7ab24cd81d6fe7b9e53370ece021a
Web D.va <webNoDva@gmail.com>
2017-08-14 12:20:42 -0400

    Player can now select boxes.

--- public_html/game.js
+++ public_html/game.js
@@ -17,7 +17,15 @@ class ExistenceBox {
             y: kontra.canvas.height / 2,
             color: 'red',
             width: WIDTH_AND_HEIGHT,
-            height: WIDTH_AND_HEIGHT
+            height: WIDTH_AND_HEIGHT,
+            isChosen: false, // A custom property
+            update: () => {
+                if (this.sprite.isChosen) {
+                    this.sprite.color = 'green';
+                } else {
+                    this.sprite.color = 'red';
+                }
+            }
         });
     }
 }
@@ -29,7 +37,26 @@ for (let i = 0; i < playingFieldArray.length; i++) {
 
 // Player's chosen box in the playing field. Initially random.
 let chosenBox = Math.floor(Math.random() * (Math.floor(playingFieldArray.length) - Math.ceil(0))) + Math.ceil(0);
-playingFieldArray[chosenBox].sprite.color = 'green';
+playingFieldArray[chosenBox].sprite.isChosen = true;
+
+// Binding keys for the player to choice a box.
+kontra.keys.bind(['a', 'left'], () => {
+    playingFieldArray[chosenBox].sprite.isChosen = false;
+    if (chosenBox === 0) {
+        chosenBox = playingFieldArray.length - 1;
+    } else {
+        chosenBox--;
+    }
+});
+
+kontra.keys.bind(['d', 'right'], () => {
+    playingFieldArray[chosenBox].sprite.isChosen = false;
+    if (chosenBox===playingFieldArray.length - 1) {
+        chosenBox = 0;
+    } else {
+        chosenBox++;
+    }
+});
 
 // Game loop object
 const mainGameLoop = kontra.gameLoop({
@@ -37,6 +64,7 @@ const mainGameLoop = kontra.gameLoop({
      * Used for controlling the game logic.
      */
     update: function () {
+        playingFieldArray[chosenBox].sprite.isChosen = true;
         // Why update the sprites that don't do anything? Can this be removed?
         playingFieldArray.forEach((item) => {
             item.sprite.update();
